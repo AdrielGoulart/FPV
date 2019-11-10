5f8941c82c9db0b81f8bb0a6b4d6633e4003d910
Adriel <adrielgoulartdeabranjo@gmail.com>
2019-11-10 12:34:56 -0300

    Arrumando a movimentação das bolas aleatoriamente

--- Códigos/script.js
+++ Códigos/script.js
@@ -146,16 +146,16 @@ function init() {
     var bolaGeometria = new THREE.SphereBufferGeometry(1, 32, 32);
 
     //Nesse for define quantas bolas serão criadas e adiciona na cena
-    for (var i = 0; i < 500; i++) {
-        var numero =  Math.random() * (1 - 0) + 0;
-        if(numero < 0.5){  
-            var bolaMaterial = new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load('../imagens/futebol.png'),specular: 0xffffff, flatShading: true});
+    for (var i = 0; i < 18; i++) {
+        var numero = Math.random() * (1 - 0) + 0;
+        if (numero < 0.5) {
+            var bolaMaterial = new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load('../imagens/futebol.png'), specular: 0xffffff, flatShading: true });
             var box = new THREE.Mesh(bolaGeometria, bolaMaterial);
-        }else{
-            var bolaMaterial = new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load('../imagens/basquete.png'),specular: 0xffffff, flatShading: true});
+        } else {
+            var bolaMaterial = new THREE.MeshPhongMaterial({ map: new THREE.TextureLoader().load('../imagens/basquete.png'), specular: 0xffffff, flatShading: true });
             var box = new THREE.Mesh(bolaGeometria, bolaMaterial);
         }
-        
+
         box.receiveShadow = true;
         box.castShadow = true;
         box.position.x = (Math.random() * 12.8 - 6.5) * 12.8;
@@ -181,21 +181,70 @@ function init() {
 
 function movBolas() {
     //Movimento das bolas
-    if (controlesAtivado) {
-        for (var i = 0; i < bolas.length; i++) {
-            delta = clock.getDelta();
-            time += delta;
-            if (i < 250) {
-               // bolas[3].position.y = 1 + Math.abs(Math.sin(time * 3)) * 15;
-                //bolas[3].position.x = Math.cos(time) * 70;
-                bolas[i].position.y = 1 + Math.abs(Math.sin(time * 3)) * 15;
-            } else {
-                //bolas[2].position.y = 1 + Math.abs(Math.sin(time * 3)) * 25;
-                bolas[i].position.y = 1 + Math.abs(Math.sin(time * 3)) * 25;
-                //bolas[2].position.z = Math.cos(time) * 70;
-            }
-        }
-    }
+
+    delta = clock.getDelta();
+    time += delta;
+
+    bolas[0].position.y = 1 + Math.abs(Math.sin(time * 3)) * 30;
+    bolas[0].position.x = Math.cos(time) * 70;
+    bolas[0].position.z = Math.sin(time) * 3;
+
+    bolas[1].position.y = 1 + Math.abs(Math.sin(time * 3)) * 8;
+    bolas[1].position.z = Math.cos(time) * 10;
+
+    bolas[2].position.y = 1 + Math.abs(Math.sin(time * 2)) * 25;
+    bolas[2].position.z = Math.cos(time) * 70;
+
+    bolas[3].position.y = 1 + Math.abs(Math.sin(time * 3)) * 15;
+    bolas[3].position.x = Math.cos(time) * 55;
+
+    bolas[4].position.y = 1 + Math.abs(Math.sin(time * 3)) * 10;
+    bolas[4].rotation.x = Math.cos(time) * 55;
+    bolas[4].position.x = Math.sin(time) * 9;
+
+    bolas[5].position.y = 1 + Math.abs(Math.sin(time * 3)) * 12;
+    bolas[5].position.x = Math.cos(time) * 18;
+    bolas[5].position.z = Math.sin(time) * 18;
+
+    bolas[6].position.y = 1 + Math.abs(Math.sin(time * 4)) * 6;
+    bolas[6].position.z = Math.cos(time) * 73;
+
+    //------------------------------------
+
+    bolas[7].position.y = 2 + Math.abs(Math.sin(time * 2.5)) * 24;
+    bolas[7].position.x = Math.cos(time) * 10.33;
+    bolas[7].position.z = Math.sin(time) * 44;
+    bolas[7].rotation.x = Math.cos(time) / 2 * 10.33;
+
+    bolas[8].position.y = 1 + Math.abs(Math.sin(time * 3)) * 21.3;
+    bolas[8].position.z = Math.cos(time) * 20;
+
+    bolas[9].position.y = 1 + Math.abs(Math.sin(time * 1.5)) * 30;
+    bolas[9].position.x = Math.sin(time) * 11.44;
+
+    bolas[10].position.y = 1 + Math.abs(Math.sin(time * 3)) * 32;
+    bolas[10].position.x = Math.sin(time) * 31;
+
+    bolas[11].position.y = 1 + Math.abs(Math.sin(time * 3)) * 36;
+    bolas[11].position.z = Math.sin(time) * 14;
+    bolas[11].rotation.z = Math.cos(time) * 13;
+
+    bolas[12].position.y = 0.7 + Math.abs(Math.sin(time * 3)) * 10.5;
+    bolas[12].position.x = (Math.sin(time) / 2) * 66;
+
+    bolas[13].position.y = 0.5 + Math.abs(Math.sin(time * 3)) * 6;
+    bolas[13].position.z = Math.sin(time) * 22;
+
+    bolas[14].position.y = 1 + Math.abs(Math.sin(time * 3)) * 12;
+    bolas[14].position.x = Math.sin(time) * 16.77;
+    bolas[14].rotation.y = Math.cos(time) * 33;
+
+    bolas[15].position.y = 2 + Math.abs(Math.sin(time * 2)) * 10.5;
+    bolas[15].position.x = (Math.cos(time) / 2) * 29;
+
+    bolas[16].position.y = 0.5 + Math.abs(Math.sin(time * 3)) * 19;
+    bolas[16].position.z = Math.sin(time) * 14.2;
+
 }
 
 function onWindowResize() {
